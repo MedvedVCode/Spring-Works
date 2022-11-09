24875a8fb29d78769d9f9a8464e3eebae7b19e73
Denis Fedorov <ssboshen@yandex.ru>
2022-11-09 16:43:19 +0300

    Add field removed to Post, add 405 exception class

--- src/main/java/ru/netology/model/Post.java
+++ src/main/java/ru/netology/model/Post.java
@@ -1,8 +1,11 @@
 package ru.netology.model;
 
+import com.google.gson.annotations.Expose;
+
 public class Post {
     private long id;
     private String content;
+    private transient boolean removed;
 
     public Post() {
     }
@@ -10,6 +13,7 @@ public class Post {
     public Post(long id, String content) {
         this.id = id;
         this.content = content;
+        this.removed = false;
     }
 
     public long getId() {
@@ -26,5 +30,13 @@ public class Post {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public boolean isRemoved() {
+        return removed;
+    }
+
+    public void setRemoved() {
+        removed = true;
     }
 }
